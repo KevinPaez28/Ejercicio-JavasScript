35e0c6a26c278a084ad516d0506aeeb95be797e3
KevinPaez28 <Kevinandreypaezgarces@gmail.com>
2024-12-02 07:32:35 -0500

    primeros 10 ejercicios

--- archivos.js/Ejercicio1.js
+++ archivos.js/Ejercicio1.js
@@ -1,7 +1,5 @@
-alert("En este archivo estan los ejercicios del 1 al 10;");
-let ejercicio = parseInt(prompt("Que ejercicio desea ejecutar?:"));
-
-if (ejercicio == 1) {
+alert("Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor o menor de edad, el programa debe validar que solo se puedan ingresar números positivos ")
+ 
   let edad = parseInt(prompt("Ingrese su edad:"))
   if (edad >= 18 && edad >= 0) {
     alert("usted es mayor de edad")
@@ -14,7 +12,6 @@ if (ejercicio == 1) {
     alert("No puede ingresar numeros negativos")
   }
     
-}
 
 
   
