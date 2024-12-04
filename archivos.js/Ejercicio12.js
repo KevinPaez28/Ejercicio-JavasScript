bffeca62285dbdcfad0e7b625d83acc3887399c0
KevinPaez28 <Kevinandreypaezgarces@gmail.com>
2024-12-04 15:47:06 -0500

    Ejercicio 12

---
+++ archivos.js/Ejercicio12.js
@@ -0,0 +1,9 @@
+alert("12. Escribir un programa que calcule el área de un rectángulo:")
+
+function area(base, altura) {
+  let area =base * altura
+  
+  alert(`El area del rectangulo es de${area}`)
+}
+let base = parseInt(prompt("Ingrese la base del rectangulo"));
+let altura = parseInt(prompt("Ingrese la altura del rectangulo"));
