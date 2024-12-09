7764a1307739b7e80eacac11542eb7ed1b67bb86
KevinPaez28 <Kevinandreypaezgarces@gmail.com>
2024-12-09 08:30:29 -0500

    Ejercicio 3

---
+++ archivos.js/ejercicio33.js
@@ -0,0 +1,9 @@
+alert("Calcular la edad de una madre en el momento que dio a luz a alguno de sus hijos.")
+
+function Edad(madre, hijo) {
+  let calculo = madre - hijo; 
+  alert(`La edad de la madre al momento de dar a luz es de ${calculo}`)
+}
+let madre = parseInt(prompt("Ingrese la edad de la madre"))
+let hijo = parseInt(prompt("Ingrese la edad del hijo"))
+Edad(madre, hijo);
