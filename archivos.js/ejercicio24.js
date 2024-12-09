03e6f2dddd6694b2260c00ad6467cd88bf7d1854
KevinPaez28 <Kevinandreypaezgarces@gmail.com>
2024-12-09 10:26:27 -0500

    Ejercicio 36

--- archivos.js/ejercicio24.js
+++ archivos.js/ejercicio24.js
@@ -1,6 +1,6 @@
 alert("Escriba una expresión regular que reconozca los números en punto flotante (por ejemplo -2.3e-1, -3e2, 23, 3.2).")
 
-const regex = /^[0-9]+([.][0-9]+)$/;
+const regex = /^[0-9-e]+([.][0-9]+)$/;
 
 function Expresion_Regular(nmr) {
   
---
+++ archivos.js/ejercicio36.js
@@ -0,0 +1,13 @@
+alert(`En el programa de cocina de “Doña Anita” han dado la receta para la preparación de
+bizcocho especial de chocolate. Por cada 100 gramos de harina hay que añadir 10 gramos
+de cacao y un puñado de nueces. Si quiero prepararlos con 20 gramos de chocolate. ¿La
+cantidad de gramos de harina para hacer el bizcocho es?`)
+
+function Ingredientes(harina , cacao){
+  receta = (harina * 2);
+
+  alert(`la cantidad de harina a añadir es de ${receta} gramos de harina`)
+}
+let harina = 100;
+let cacao = 10;
+Ingredientes(harina , cacao)
