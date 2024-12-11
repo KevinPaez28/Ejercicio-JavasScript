973e7b18284e2bc82f7c0654c266da2020905b06
KevinPaez28 <Kevinandreypaezgarces@gmail.com>
2024-12-10 19:21:10 -0500

    Ejercicio 65

---
+++ archivos.js/ejercicio65.js
@@ -0,0 +1,14 @@
+alert(`. Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla
+la cuenta atrás desde ese número hasta cero separados por comas.`)
+
+function Regresiva(numero) {
+  let numeros = [];
+  if (numero >= 0) {
+    for (numero; numero >= 0; numero--){
+      numeros.push(numero);
+    }
+  }
+  alert(numeros)
+} 
+let numero = parseInt(prompt("Ingrese un numero"))
+Regresiva(numero)
