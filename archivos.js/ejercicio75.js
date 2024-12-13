c013f4b60a53a1bf0c0fff9e694145017319ee28
KevinPaez28 <Kevinandreypaezgarces@gmail.com>
2024-12-12 20:19:23 -0500

    Ejercicio 75

--- archivos.js/ejercicio75.js
+++ archivos.js/ejercicio75.js
@@ -1,17 +1,14 @@
 alert(`Crea una función que calcule la longitud de una palabra si es corta o si es larga. (palabras
 cortas menores de 5 caracteres).`)
 
-function Longitud(palabra) {
-  for (let i = 0; i < palabra.length; i++){
-    if (i > 5) {
+function Longitud(palabras) {
+    if (palabras.length > 5) {
       alert("La palabra es larga")
-      break
-    } else {
+    }else{
       alert("la palabra es corta")
-      break
     }
     
   }
-}
-let palabra=prompt("Ingrese una para saber si es larga o corta")
-Longitud(palabra)
+
+let palabras=prompt("Ingrese una para saber si es larga o corta")
+Longitud(palabras)
