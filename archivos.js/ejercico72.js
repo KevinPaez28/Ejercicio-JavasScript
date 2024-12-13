36d81d9dc26940c4b99a922f0e9971597ea48723
KevinPaez28 <Kevinandreypaezgarces@gmail.com>
2024-12-12 19:38:15 -0500

    Ejercicio 72

---
+++ archivos.js/ejercico72.js
@@ -0,0 +1,16 @@
+alert(`4. Escribir un programa que muestre el eco de todo lo que el usuario introduzca hasta que el
+usuario escriba “salir” que terminará`)
+
+
+
+let palabras = [];
+let palabra;
+
+do {
+  palabra = prompt("Ingrese palabras, para terminar ingrese 'salir':");
+  if (palabra != "salir") {
+    palabras.push(palabra);
+  }
+} while (palabra !== "salir");
+
+alert(`Las palabras ingresadas fueron: ${palabras.join(", ")}`);
